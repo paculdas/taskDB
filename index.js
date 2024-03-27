25564aee55365ef9f0b8aee8f3f61db2e617e153
paculdas <sadlucap.cristyjones@gmail.com>
2024-03-28 00:54:31 +0800

    small edits

--- index.js
+++ index.js
@@ -15,6 +15,14 @@ const connectDB = require('./connectMongo');
 const User = require('./models/user');
 const Todo = require('./models/todo');
 
+const generateSecretKey = () => {
+    const secretKey = crypto.randomBytes(32).toString("hex");
+  
+    return secretKey;
+  };
+
+const secretKey = generateSecretKey();
+
 app.get('/api/users', async (req, res) => {
     try {
         const users = await User.find();
@@ -33,6 +41,33 @@ app.get('/api/todos/:userId', async (req, res) => {
     } catch (error) {
         return res.status(500).json({ error: error.message });
     }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid Email" });
+    }
+
+    if (user.password !== password) {
+      return res.status(401).json({ message: "Invalid Password" });
+    }
+
+    jwt.sign({ userId: user._id }, secretKey, (error, token) => {
+      if (error) {
+        console.error("Error generating token:", error);
+        return res.status(500).json({ message: "Token generation failed" });
+      }
+      console.log("Token generated successfully:", token);
+      res.status(200).json({ token, userId: user._id });
+    });
+  } catch (error) {
+    console.log("Login failed", error);
+    res.status(500).json({ message: "Login failed" });
+  }
 });
 
 connectDB(); 
